d5f1d5da6a51fe6d9e1bd1d1fd3f95f85d181a37
Anindya Mukherjee <andysenclave@Andys-MacBook-Pro.local>
2025-07-26 00:50:51 +0530

    🔐 Auto-provision user from JWT via JwtStrategy

--- apps/user-service/src/user/user.module.ts
+++ apps/user-service/src/user/user.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-
+import { AuthModule } from 'src/auth/auth.module';
+import { PrismaService } from '@kriyo/db';
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, PrismaService],
 })
 export class AppModule {}
