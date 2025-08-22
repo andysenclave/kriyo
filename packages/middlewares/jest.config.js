8b5e7a57a4e34980ce41f8beccda23392e2f0acb
Anindya Mukherjee <andysenclave@Andys-MacBook-Pro.local>
2025-08-22 13:49:13 +0530

    feat(origin-validator): implement more security for core services, now requests from only allowed origins are authorized to access data

---
+++ packages/middlewares/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.ts', '!**/*.d.ts', '!**/node_modules/**', '!**/dist/**'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  setupFilesAfterEnv: [],
+  testTimeout: 10000,
+};
