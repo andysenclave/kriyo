8aaefb5d1001da428ce0c4ac109dd117e18d0225
Anindya Mukherjee <andysenclave@Andys-MacBook-Pro.local>
2025-08-22 13:49:54 +0530

    feat(origin-validator): implement more security for core services, now requests from only allowed origins are authorized to access data

--- apps/tasks-service/src/app.module.ts
+++ apps/tasks-service/src/app.module.ts
@@ -2,7 +2,10 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
-import { ClientIdValidatorMiddleware } from '@kriyo/middlewares';
+import {
+  ClientIdValidatorMiddleware,
+  OriginValidatorMiddleware,
+} from '@kriyo/middlewares';
 import 'dotenv/config';
 
 @Module({
@@ -11,12 +14,18 @@ import 'dotenv/config';
 })
 export class AppModule implements NestModule {
   ALLOWED_CLIENT_IDS = process.env.ALLOWED_CLIENT_IDS?.split(',') || [];
+  ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS?.split(',') || [];
   configure(consumer: MiddlewareConsumer) {
     const clientIdMiddleware = new ClientIdValidatorMiddleware();
+    const originValidatorMiddleware = new OriginValidatorMiddleware();
     clientIdMiddleware.setAllowedClientIds(this.ALLOWED_CLIENT_IDS);
+    originValidatorMiddleware.setAllowedOrigins(this.ALLOWED_ORIGINS);
 
     consumer
-      .apply(clientIdMiddleware.use.bind(clientIdMiddleware))
+      .apply(
+        clientIdMiddleware.use.bind(clientIdMiddleware),
+        originValidatorMiddleware.use.bind(originValidatorMiddleware),
+      )
       .forRoutes('*');
   }
 }
